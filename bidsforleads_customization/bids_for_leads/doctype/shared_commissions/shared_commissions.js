0e5d7ed078b168ddbd9e558f866ec51c30654df6
Akshay-Syscort <akshay.shaharwale@syscort.com>
2025-01-31 15:57:33 +0530

    Shared Commissions Doctype

---
+++ bidsforleads_customization/bids_for_leads/doctype/shared_commissions/shared_commissions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Akshay and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Shared Commissions", {
+// 	refresh(frm) {
+
+// 	},
+// });
